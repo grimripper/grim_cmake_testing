1975962b69c4ef49895580d9ac14c53b68a558ad
Vaibhav Thukral <vthukral@nvidia.com>
2018-05-26 17:11:17 -0700

    update

---
+++ Test101/build/CMakeFiles/vector-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector-test.dir/array.cpp.o"
+  "CMakeFiles/vector-test.dir/vector.cpp.o"
+  "CMakeFiles/vector-test.dir/main.cpp.o"
+  "vector-test.pdb"
+  "vector-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
